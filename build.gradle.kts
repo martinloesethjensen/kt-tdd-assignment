6893e5fa4b80029240714c987aedf5fdd3c4e4bf
Martin L. Jensen <martinloesethjensen@gmail.com>
2020-04-18 12:43:04 +0200

    Init

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    java
+    kotlin("jvm") version "1.3.72"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    testImplementation(group = "org.junit.jupiter", name = "junit-jupiter", version = "5.5.0")
+    testImplementation("org.amshove.kluent:kluent:1.4")
+    testImplementation("io.mockk:mockk:1.9.3")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
